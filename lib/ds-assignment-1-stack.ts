88e8261b8ed842f12661ac16c5a3e28785ead7df
Evan Hearne <evanhearne02@gmail.com>
2024-11-07 12:28:45 +0000

    add dynamodb tables + composite primary key

--- lib/ds-assignment-1-stack.ts
+++ lib/ds-assignment-1-stack.ts
@@ -1,16 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class DsAssignment1Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Stock Table
+    const stockTable = new dynamodb.Table(this, 'StockTable', {
+      partitionKey: { name: 'IceCreamID', type: dynamodb.AttributeType.NUMBER },
+      tableName: 'Stock',
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DsAssignment1Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Customer Table
+    const customerTable = new dynamodb.Table(this, 'CustomerTable', {
+      partitionKey: { name: 'CustomerID', type: dynamodb.AttributeType.NUMBER }, 
+      sortKey: { name: 'Name', type: dynamodb.AttributeType.STRING }, // Composite key
+      tableName: 'Customer',
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
   }
 }
