5f4599d5ea631b773cf1b9676aeb9f629f790867
Evan Hearne <evanhearne02@gmail.com>
2024-11-09 11:02:50 +0000

    add auth stack code

--- lib/auth-stack.ts
+++ lib/auth-stack.ts
@@ -1,5 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
 
 export class AuthStack extends cdk.Stack {
+  public readonly userPool: cognito.UserPool;
+  public readonly userPoolClient: cognito.UserPoolClient;
 
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create a Cognito User Pool
+    this.userPool = new cognito.UserPool(this, 'UserPool', {
+      selfSignUpEnabled: true,
+      signInAliases: { email: true },
+      autoVerify: { email: true },
+      passwordPolicy: {
+        minLength: 6,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireDigits: true,
+        requireSymbols: false,
+      },
+    });
+
+    // Create a User Pool Client
+    this.userPoolClient = new cognito.UserPoolClient(this, 'UserPoolClient', {
+      userPool: this.userPool,
+      generateSecret: false,
+    });
+  }
 }
